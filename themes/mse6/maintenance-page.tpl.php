fb680b8ed87cb0ff91632163f731eb5d6445abba
Callin Mullaney <callin@callinmullaney.me>
2014-12-08 23:11:18 -0600

    add custom dropdown. more styling for an animated background.

--- themes/mse6/maintenance-page.tpl.php
+++ themes/mse6/maintenance-page.tpl.php
@@ -6,7 +6,6 @@
     <?php print $head; ?>
     <link href="http://fonts.googleapis.com/css?family=Lato:400,300,300italic,700,400italic,700italic" rel="stylesheet" type="text/css">
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-    <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
     <?php print $styles; ?>
     <?php print $scripts; ?>
   </head>
@@ -42,6 +41,97 @@
   </div>
 
   <?php print $page_bottom; ?>
+
+<canvas id="processing"></canvas>
+
+<script type="application/processing">
+int fc, num = 300, edge = 0;
+ArrayList ballCollection;
+boolean save = false;
+ 
+void setup() {
+  size(2560, 1440);
+  background(32,32,32);
+  ballCollection = new ArrayList();
+  createStuff();
+}
+ 
+void draw() {
+  background(32,32,32);
+  for (int i=0; i<ballCollection.size(); i++) {
+    Ball mb = (Ball) ballCollection.get(i);
+    mb.run();
+  }
+}
+ 
+void keyPressed() {
+  fc = frameCount;
+  save = true;
+}
+ 
+void createStuff() {
+  ballCollection.clear();
+  for (int i=0; i<num; i++) {
+    PVector org = new PVector(random(edge, width-edge), random(edge, height-edge));
+    float radius = random(10, 80);
+    PVector loc = new PVector(org.x+radius, org.y);
+    float offSet = random(TWO_PI);
+    int dir = 1;
+    float r = random(1);
+    if (r>.5) dir =-1;
+    Ball myBall = new Ball(org, loc, radius, dir, offSet);
+    ballCollection.add(myBall);
+  }
+}
+ 
+ 
+class Ball {
+ 
+  PVector org, loc;
+  float sz = 10;
+  float theta, radius, offSet;
+  int s, dir, d = 120;
+ 
+  Ball(PVector _org, PVector _loc, float _radius, int _dir, float _offSet) {
+    org = _org;
+    loc = _loc;
+    radius = _radius;
+    dir = _dir;
+    offSet = _offSet;
+  }
+ 
+  void run() {
+    display();
+    move();
+    lineBetween();
+  }
+ 
+  void move() {
+    loc.x = org.x + sin(theta+offSet)*radius;
+    loc.y = org.y + cos(theta+offSet)*radius;
+    theta += (0.075/3*dir);
+  }
+ 
+  void lineBetween() {
+    for (int i=0; i<ballCollection.size(); i++) {
+      Ball other = (Ball) ballCollection.get(i);
+      float distance = loc.dist(other.loc);
+      if (distance >0 && distance < d) {
+        stroke(#ffffff,150);
+        line(loc.x, loc.y, other.loc.x, other.loc.y);
+      }
+    }
+  }
+ 
+  void display() {
+    noStroke();
+    for (int i=0; i<5; i++) {
+      fill(255);
+      ellipse(loc.x, loc.y, sz, sz);
+    }
+  }
+}
+</script>
 
   </body>
 </html>
--- themes/mse6/template.php
+++ themes/mse6/template.php
@@ -1,41 +1,6 @@
-<?php
+<?php 
 
-/**
- * @file
- * This file contains the main theme functions hooks and overrides.
- */
-
-/**
- * Override or insert variables into the maintenance page template.
- */
-function mse6_preprocess_maintenance_page(&$vars) {
-  // While markup for normal pages is split into page.tpl.php and html.tpl.php,
-  // the markup for the maintenance page is all in the single
-  // maintenance-page.tpl.php template. So, to have what's done in
-  // mse6_preprocess_html() also happen on the maintenance page, it has to be
-  // called here.
-  mse6_preprocess_html($vars);
-}
-
-/**
- * Override or insert variables into the html template.
- */
-function mse6_preprocess_html(&$vars) {
-
-  // Get mse6 folder path.
-  $mse6_path = drupal_get_path('theme', 'mse6');
-
-  // Add conditional CSS for IE8 and below.
-  drupal_add_css($mse6_path . '/css/style.css', array('group' => CSS_THEME, 'weight' => 999, 'preprocess' => FALSE));
-
-  // Fix the viewport and zooming in mobile devices.
-  $viewport = array(
-   '#tag' => 'meta',
-   '#attributes' => array(
-     'name' => 'viewport',
-     'content' => 'width=device-width, maximum-scale=1, minimum-scale=1, user-scalable=no, initial-scale=1',
-   ),
-  );
-  drupal_add_html_head($viewport, 'viewport');
-
+function mse6_js_alter(&$javascript) {
+  // Swap out jQuery to use an updated version of the library.
+  $javascript['misc/jquery.js']['data'] = 'http://code.jquery.com/jquery-1.8.3.min.js';
 }
